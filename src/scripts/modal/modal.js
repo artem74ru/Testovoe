acbbd97cf8fd3eaf1f7c85843c8648b40da476cd
Artem Mitrofanov <artem.bvdf@mail.ru>
2024-10-15 21:32:11 +0500

    fourth  commit

--- src/scripts/modal/modal.js
+++ src/scripts/modal/modal.js
@@ -1,6 +1,16 @@
 const button = document.querySelector("button.footer-top__button");
 const modal = document.querySelector("div.modal-disable");
+const close = document.querySelector("img.modal-close__img");
 
 button.addEventListener("click", () => {
     modal.classList.add("modal-active");
+    
 });
+
+window.addEventListener("click", function(event) {
+  if (event.target == close) {
+    modal.classList.remove("modal-active");
+  }
+});
+
+
